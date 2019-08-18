cf091ef23968743876e60fe52eb86a5cd489b737
Patipon Riebpradit <patipon74@gmail.com>
2019-08-18 21:13:31 +0200

    [+, PARENT] added profile screen

--- screens/parent/GradeScreen.js
+++ screens/parent/GradeScreen.js
@@ -1,24 +1,127 @@
 import React from "react"
-import { ScrollView, StyleSheet } from "react-native"
-import { ExpoLinksView } from "@expo/samples"
+import { ScrollView, StyleSheet, View, Text } from "react-native"
+import TouchableScale from "react-native-touchable-scale"
+import { ListItem } from "react-native-elements"
 
-export default function GradeScreen() {
+const data = [
+     {
+          title: "Science",
+          subtitle: "Prof. John Doe",
+          score: 4
+     },
+     {
+          title: "Mathematics",
+          subtitle: "Prof. John Doe",
+
+          score: 2
+     },
+     {
+          title: "Mathematics",
+          subtitle: "Prof. John Doe",
+
+          score: 1
+     },
+     {
+          title: "Mathematics",
+          subtitle: "Prof. John Doe",
+
+          score: "R"
+     },
+     {
+          title: "Science",
+          subtitle: "Prof. John Doe",
+          score: 4
+     },
+     {
+          title: "Science",
+          subtitle: "Prof. John Doe",
+          score: 4
+     },
+     {
+          title: "Mathematics",
+          subtitle: "Prof. John Doe",
+
+          score: 2
+     }
+]
+
+const dataStyle = [
+     ["#FFD200", "#F7971E"],
+     ["#38ef7d", "#11998e"],
+     ["#EB5757", "#000000"]
+]
+
+export default (GradeScreen = () => {
      return (
           <ScrollView style={styles.container}>
-               {/**
-                * Go ahead and delete ExpoLinksView and replace it with your content;
-                * we just wanted to provide you with some helpful links.
-                */}
-               <ExpoLinksView />
+               <View style={styles.agendaContainer}>
+                    {data.map((element, i) => (
+                         <ListItem
+                              containerStyle={styles.listItemStyle}
+                              key={i}
+                              Component={TouchableScale}
+                              friction={90}
+                              tension={100}
+                              activeScale={0.95}
+                              linearGradientProps={{
+                                   colors: getColorScheme(element.score),
+                                   start: [1, 0],
+                                   end: [0.2, 0]
+                              }}
+                              leftIcon={{ name: "assignment" }}
+                              title={element.title}
+                              titleStyle={{
+                                   color: "white",
+                                   fontWeight: "bold"
+                              }}
+                              rightElement={() => (
+                                   <Text style={styles.scoreStyle}>
+                                        {element.score}
+                                   </Text>
+                              )}
+                              subtitleStyle={{ color: "white" }}
+                              subtitle={element.subtitle}
+                         />
+                    ))}
+               </View>
+               <View />
           </ScrollView>
      )
+})
+
+const getColorScheme = score => {
+     if (score >= 3) return dataStyle[1]
+     else if (score >= 2) return dataStyle[0]
+     else return dataStyle[2]
 }
 
 GradeScreen.navigationOptions = {
-     title: "Links"
+     title: "ผลการเรียน"
 }
 
 const styles = StyleSheet.create({
+     item: {
+          backgroundColor: "white",
+          flex: 1,
+          borderRadius: 5,
+          padding: 10,
+          marginRight: 10,
+          marginTop: 17
+     },
+     listItemStyle: {
+          borderRadius: 15,
+          marginBottom: 16
+     },
+     agendaContainer: {
+          marginLeft: 16,
+          marginRight: 16
+     },
+     scoreStyle: {
+          fontWeight: "bold",
+          fontSize: 18,
+          marginRight: 16
+     },
+
      container: {
           flex: 1,
           paddingTop: 15,
--- screens/parent/ProfileScreen.js
+++ screens/parent/ProfileScreen.js
@@ -1,27 +1,119 @@
 import React from "react"
-import { ScrollView, StyleSheet } from "react-native"
-import { ExpoLinksView } from "@expo/samples"
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native"
+import { Avatar, ListItem } from "react-native-elements"
+import { TouchableScale } from "react-native-touchable-scale"
 
 export default function ProfileScreen() {
      return (
-          <ScrollView style={styles.container}>
-               {/**
-                * Go ahead and delete ExpoLinksView and replace it with your content;
-                * we just wanted to provide you with some helpful links.
-                */}
-               <ExpoLinksView />
-          </ScrollView>
+          <View style={styles.container}>
+               <View style={styles.header} />
+               <Image
+                    style={styles.avatar}
+                    source={{
+                         uri:
+                              "https://bootdey.com/img/Content/avatar/avatar6.png"
+                    }}
+               />
+               <View style={styles.body}>
+                    <View style={styles.bodyContent}>
+                         <Text style={styles.name}> ปฏิพล เรียบประดิษฐ์ </Text>
+                         <Text style={styles.info}>ม. 4/8 เลขที่ 24</Text>
+                    </View>
+                    <View>
+                         <ListItem
+                              title={"รหัสนักเรียน"}
+                              titleStyle={{ fontSize: 18, fontWeight: "bold" }}
+                              containerStyle={{
+                                   marginLeft: 16,
+                                   marginRight: 16,
+                                   marginTop: 64
+                              }}
+                              rightTitle="12654"
+                              rightTitleStyle={{ fontSize: 18 }}
+                         />
+                         <ListItem
+                              title={"การบ้าน"}
+                              titleStyle={{ fontSize: 18, fontWeight: "bold" }}
+                              containerStyle={{
+                                   marginLeft: 16,
+                                   marginRight: 16
+                              }}
+                              rightTitle="22"
+                              rightTitleStyle={{ fontSize: 18 }}
+                         />
+                         <ListItem
+                              title={"เข้าเรียน"}
+                              titleStyle={{ fontSize: 18, fontWeight: "bold" }}
+                              containerStyle={{
+                                   marginLeft: 16,
+                                   marginRight: 16
+                              }}
+                              rightTitle="20%"
+                              rightTitleStyle={{ fontSize: 18 }}
+                         />
+                    </View>
+               </View>
+          </View>
      )
 }
 
 ProfileScreen.navigationOptions = {
-     title: "Links"
+     header: null
 }
-
 const styles = StyleSheet.create({
-     container: {
+     header: {
+          backgroundColor: "#00BFFF",
+          height: 200
+     },
+     avatar: {
+          width: 130,
+          height: 130,
+          borderRadius: 63,
+          borderWidth: 4,
+          borderColor: "white",
+          marginBottom: 10,
+          alignSelf: "center",
+          position: "absolute",
+          marginTop: 130
+     },
+     name: {
+          fontSize: 22,
+          color: "#FFFFFF",
+          fontWeight: "600"
+     },
+     body: {
+          marginTop: 40
+     },
+     bodyContent: {
           flex: 1,
-          paddingTop: 15,
-          backgroundColor: "#fff"
+          alignItems: "center",
+          padding: 30
+     },
+     name: {
+          fontSize: 28,
+          color: "#696969",
+          fontWeight: "600"
+     },
+     info: {
+          fontSize: 16,
+          color: "#00BFFF",
+          marginTop: 6
+     },
+     description: {
+          fontSize: 16,
+          color: "#696969",
+          marginTop: 8,
+          textAlign: "center"
+     },
+     buttonContainer: {
+          marginTop: 10,
+          height: 45,
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 20,
+          width: 250,
+          borderRadius: 30,
+          backgroundColor: "#00BFFF"
      }
 })
